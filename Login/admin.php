b1d0b1c363218e1909069d0a9c156480c22e65c9
Filleral <filleral19@gmail.com>
2022-02-05 09:48:45 -0500

    dia 5

--- Login/admin.php
+++ Login/admin.php
@@ -142,7 +142,7 @@
                 <?php endif;?>
                 <?php echo remove_junk(first_character($recent_product['name']));?>
                   <span class="label label-warning pull-right">
-                 $<?php echo (int)$recent_product['sale_price']; ?>
+                 #<?php echo (int)$recent_product['sale_price']; ?>
                   </span>
                 </h4>
                 <span class="list-group-item-text pull-right">
